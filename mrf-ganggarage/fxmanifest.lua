7d8035e9065e8a8489f2166606a71ebf44d4eaba
MARFY <118571529+M4RFY@users.noreply.github.com>
2022-11-22 18:03:47 +0600

    Add files via upload

---
+++ mrf-ganggarage/fxmanifest.lua
@@ -0,0 +1,12 @@
+name "MRF-GARAGEGANG"
+author "MARFY"
+version "v1.0.0"
+description "Gang Garage Script By Marfy"
+fx_version "cerulean"
+game "gta5"
+
+client_scripts { 'client.lua', }
+server_scripts { 'server.lua', }
+shared_scripts { 'config.lua', 'functions.lua' }
+
+lua54 'yes'
